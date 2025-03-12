2890a552fd6db7443e2088c5981161c7b92e3b7e
Alexis <saltetalexis@gmail.com>
2025-03-12 10:01:35 +0100

    typography

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,15 @@
-<h1><?= $page->title() ?></h1>
+<?php snippet('head') ?>
+
+    <!-- <h1><?= $page->title() ?></h1> -->
+     <main>
+     <img src="<?= url('assets/img/ui/logo.svg') ?>?v=<?= time() ?>" alt="CENT 15 ARCHITECTURE">
+        <p class="active"><span class="mono">[</span><span class="glyph">&nbsp;&nbsp;MENU&nbsp;&nbsp;</span><span class="mono">]</span>
+         <p class="active"><span class="mono">[</span><span class="glyph">X</span><span class="mono">]</span> TOUT</p>
+         <p><span class="mono">[</span><span class="glyph">X</span><span class="mono">]</span> LOGEMENT</p>
+         <p><span class="mono">[</span><span class="glyph">X</span><span class="mono">]</span> TERTIAIRE</p>
+         <p><span class="mono">[</span><span class="glyph">X</span><span class="mono">]</span> CULTURE</p>
+         <p class="active"><span class="mono">[</span><span class="glyph">/</span><span class="mono">]</span>
+         <p>-------</p>
+         <p>0810 // Salle de spectacle / trianon / restaurer</p>
+     </main>
+</body>
